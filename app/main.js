f1e86f20e111c60a8b2ce71f99211cf62c6aea91
Rodolfo Patane <rodolfo@patane.dev>
2022-01-28 15:55:53 -0300

    implement dapp integration, and  improvement contract

--- app/main.js
+++ app/main.js
@@ -1,6 +1,8 @@
+// MESSAGES
+const NOT_CONECTED = 'Connect your wallet first';
 // add
 const infuraAPIKey = '';
-const contractAdress = '0xFd545F40f6f924437EBb2eD95293d8AC18C8e51a';
+const contractAdress = '0x0E2aABA6E3210EA3F07dC75446778CAE55DA3Eed';
 const provider = `wss://rinkeby.infura.io/ws/v3/${infuraAPIKey}`;
 
 // buttons
@@ -8,21 +10,26 @@ const connectWalletButton = document.getElementById('connectWalletButton');
 const disconnectWalletButton = document.getElementById('disconnectWalletButton');
 const includeProposalButton = document.getElementById('includeProposalButton');
 const autorizeProposeButton = document.getElementById('autorizeProposeButton');
+const autorizeUserToVoteInProposalButton = document.getElementById('autorizeUserToVoteInProposalButton');
 
 // display results/errors
 const selectedAccountDisplay = document.getElementById('selectedAccountDisplay');
 const sendProposalResultDisplay = document.getElementById('sendProposalResultDisplay');
-const autorizeProposeDisplay = document.getElementById('listProposalsDiv');
-const listProposalsDiv = document.getElementById('autorizeProposeDisplay');
+const listProposalsDiv = document.getElementById('listProposalsDiv');
+const autorizeProposeDisplay = document.getElementById('autorizeProposeDisplay');
+const voteDisplay = document.getElementById('voteDisplay');
+
 
 // form field
 const proposalDescription = document.getElementById('proposalDescription');
 const autorizeUserInput = document.getElementById('autorizeUserInput');
-
+const autorizeUserToVoteInProposalInput = document.getElementById('autorizeUserToVoteInProposalInput');
+const autorizeUserToVoteInProposalSelect = document.getElementById('autorizeUserToVoteInProposalSelect');
 // global info
 let selectedAccount;
 let web3;
 let contract;
+let proposalVotes;
 
 // connect wallete functions
 
@@ -53,17 +60,13 @@ const disconnect = async () => {
     disconnectWalletButton.hidden = true
 
 }
+
 const checkConnectedWallet = async () => {
     if (selectedAccount) {
         return true;
     }
-
-    if (await connect()) {
-        return true;
-    }
-
+    alert(NOT_CONECTED);
     return false
-
 }
 
 // event buttons
@@ -82,8 +85,11 @@ includeProposalButton.addEventListener("click", () => {
 autorizeProposeButton.addEventListener("click", () => {
     autorizePropose();
 });
+autorizeUserToVoteInProposalButton.addEventListener("click", () => {
+    autorizeUserToVoteInProposal();
+});
 
-// load initial state
+// load initial state and wallet status subscribe
 window.onload = () => {
     const time = setTimeout(async () => {
         clearTimeout(time);
@@ -102,19 +108,31 @@ window.onload = () => {
         const abiInterface = (await file.json()).abi;
         contract = new web3.eth.Contract(abiInterface, contractAdress);
         getProposalVotes();
+        startListeningWalletChanges();
     }, 200);
 
 }
 
-/// contract interact functions
-const includeProposal = async () => {
+const startListeningWalletChanges = async () => {
+    window.ethereum.on('accountsChanged', (wallets) => {
+        if (window.ethereum.selectedAddress !== selectedAccount) {
+            selectedAccount = window.ethereum.selectedAddress;
+            window.localStorage.setItem('wallet', selectedAccount)
+            selectedAccountDisplay.innerHTML = selectedAccount;
+            return;
+        }
+        if (wallets.length === 0) {
+            disconnect();
+        }
+    })
+}
+
+// generate TX object
+const generateAndSendTxObject = async (method, ...data) => {
     if (await checkConnectedWallet()) {
-        const _proposalDescription = proposalDescription.value;
         try {
-            const transactionData = await contract.methods.includeProposal(_proposalDescription).encodeABI();
-
+            const transactionData = await contract.methods[method](...data).encodeABI();
             txCount = await web3.eth.getTransactionCount(selectedAccount);
-
             const txObject = {
                 to: contractAdress,
                 from: selectedAccount,
@@ -127,56 +145,52 @@ const includeProposal = async () => {
                 method: 'eth_sendTransaction',
                 params: [txObject]
             });
-            sendProposalResultDisplay.innerHTML = `${txHash}`;
+            return txHash;
         } catch (error) {
-            sendProposalResultDisplay.innerHTML = error.message;
+            return error.message;
         }
+    } else {
+        return NOT_CONECTED;
     }
+};
 
+/// contract interact functions
+const getProposalVotes = async () => {
+    try {
+        proposalVotes = await contract.methods.getProposalVotes().call();
+        proposalVotes.forEach((proposal, index) => {
+            const p = document.createElement(`p`);
+            p.innerHTML = `<p class="mb-0">Voltes: ${proposal[1]} -  Proposal: ${proposal[0]}</p><button class="btn btn-outline-success me-2" onclick="vote(${index})">Vote</button>`;
+            listProposalsDiv.appendChild(p);
+
+            const option = document.createElement(`option`);
+            option.innerHTML = `<option value="${index}">${proposal[0]}</option>`;
+            autorizeUserToVoteInProposalSelect.appendChild(option);
+
+        });
+        console.log(proposalVotes);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const includeProposal = async () => {
+    sendProposalResultDisplay.innerHTML = await generateAndSendTxObject('includeProposal', proposalDescription.value);
 }
 
 const autorizePropose = async () => {
-    if (await checkConnectedWallet()) {
-        const _autorizeUserInput = autorizeUserInput.value;
-        try {
-            const transactionData = await contract.methods.autorizePropose(_autorizeUserInput).encodeABI();
-
-            txCount = await web3.eth.getTransactionCount(selectedAccount);
-
-            const txObject = {
-                to: contractAdress,
-                from: selectedAccount,
-                nonce: web3.utils.toHex(txCount),
-                gasLimit: web3.utils.toHex(2100000),
-                gasPrice: web3.utils.toHex(await web3.eth.getGasPrice()),
-                data: transactionData
-            }
-            const txHash = await window.ethereum.request({
-                method: 'eth_sendTransaction',
-                params: [txObject]
-            });
-            autorizeProposeDisplay.innerHTML = `${txHash}`;
-        } catch (error) {
-            autorizeProposeDisplay.innerHTML = error.message;
-        }
-    }
+    autorizeProposeDisplay.innerHTML = await generateAndSendTxObject('autorizePropose', autorizeUserInput.value);
 }
 
-const getProposalVotes = async () => {
-    if (await checkConnectedWallet()) {
-        try {
-            const proposalVotes = await contract.methods.getProposalVotes().call();
-            proposalVotes.forEach(proposal => {
-                const p = document.createElement(`p`);
-                p.innerHTML = `<p class="mb-0">${proposal}</p>`;
-                listProposalsDiv.appendChild(p);
-            });
-
-
-
-            console.log(proposalVotes);
-        } catch (error) {
-            console.log(error);
-        }
+const autorizeUserToVoteInProposal = async () => {
+    const _selectedProposal = autorizeUserToVoteInProposalSelect.selectedIndex - 1;
+    if (_selectedProposal < 0) {
+        alert('Select an proposal')
+        return;
     }
+    autorizeUserToVoteInProposalDisplay.innerHTML = await generateAndSendTxObject('autorizeUserToVoteInProposal', _selectedProposal, selectedAccount);
+}
+
+const vote = async (_proposal) => {
+    voteDisplay.innerHTML = await generateAndSendTxObject('vote', _proposal);
 }
